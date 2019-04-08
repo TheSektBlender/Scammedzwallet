d461be658c3c44dcd3d3a7dd5f441877606dadca
TheSektBlender <49373110+TheSektBlender@users.noreply.github.com>
2019-04-08 13:43:56 +0100

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "scammerzcoin")
+set(CN_CURRENCY_DISPLAY_NAME "ScammerzCoin")
+set(CN_CURRENCY_TICKER "SZC")
